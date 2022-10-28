02c2653b63d985118224004fd07296d7ff0b41e7
ibro001 <ibrahimmoses56@gmail.com>
2022-10-28 19:12:42 +0100

    created the portfolio and testimonial dataBase

---
+++ src/DB/portfolioDB.js
@@ -0,0 +1,28 @@
+import IMG4 from '../assets/IMG4.png'
+import IMG5 from '../assets/IMG5.png'
+import IMG6 from '../assets/IMG6.png'
+
+
+export const portfolioDB =  [
+    {
+        id: 1,
+        image: IMG4,
+        title: 'Fullstack Music Player App',
+        github: 'https://github.com/Ibro001/Music-Player-App',
+        demo: 'https://my-music-app-clone.netlify.app/'
+    },
+    {
+        id: 2,
+        image: IMG5,
+        title: 'Fullstack Amazon Clone',
+        github: 'https://github.com/Ibro001/Amazon-clone',
+        demo: 'https://my-amazon-clone-app.netlify.app/'
+    },
+    {
+        id: 3,
+        image: IMG6,
+        title: 'React Hulu Clone',
+        github: 'https://github.com/Ibro001/hulu-clone',
+        demo: 'https://my-hulu-clone-app.netlify.app/'
+    },
+]
---
+++ src/DB/testimonialDB.js
@@ -0,0 +1,24 @@
+import avatar1 from '../assets/avatar1.jpg'
+import avatar2 from '../assets/avatar2.jpg'
+import avatar3 from '../assets/avatar3.jpg'
+
+export const testimonialDB = [
+    {
+        id: 1,
+        image: avatar1 ,
+        name: 'Ali Gee',
+        review: 'A great dev and a fast self learner who has the end user in focus.',
+    },
+    {
+        id: 2,
+        image: avatar2,
+        name: 'Icon Beatz',
+        review: 'A great engineer who knows how to get the job done 100%',
+    },
+    {
+        id: 3,
+        image:avatar3,
+        name: 'Mafeng Davou',
+        review: 'I have been working with Moses for about 3 years now. He is a brilliant engineer, passionate about software and committed to delivering high-quality solutions. With his experience and versatile technology knowledge  he really helped me grow as a developer and improve my skills.'
+    },
+]
